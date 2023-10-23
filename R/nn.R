b3de854300923fce5f3bd9ebef9ca958f8e0aed1
Michail Tsagris <mtsagris@yahoo.gr>
2023-10-23 18:21:40 +0300

    change

--- R/nn.R
+++ R/nn.R
@@ -1,7 +1,5 @@
-
-
 nn <- function(data, points = data, k = nrow(data), search = "standard", eps = 0.0, 
-                sorted = FALSE, radius = 0.0, trans = TRUE, parallel = FALSE){
+                sorted = FALSE, radius = 0.0, trans = TRUE, parallel = FALSE) {
     res <- .Call(`_Rnanoflann_knn`, t(data), t(points), k, search, eps, sorted, radius, parallel)
     if(trans){
         res$indices <- t(res$indices)
