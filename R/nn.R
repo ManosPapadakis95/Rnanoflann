1308c2ae5e2cc4a1dd78fc652de5ed574de0f919
Manos Papadakis <papadakm95@gmail.com>
2023-10-25 16:15:20 +0300

    Update nn.R

--- R/nn.R
+++ R/nn.R
@@ -1,6 +1,7 @@
-nn <- function(data, points = data, k = nrow(data), method = "euclidean", search = "standard", eps = 0.0, 
+nn <- function(data, points = data, k = nrow(data), method = "euclidean", search = "standard", eps = 0.0, square = FALSE,
                 sorted = FALSE, radius = 0.0, trans = TRUE, leafs = 10, parallel = FALSE, cores = 0){
-    res <- .Call(`_Rnanoflann_nn`, t(data), t(points), k, method, search, eps, sorted, radius, leafs, parallel, cores)
+    res <- .Call(`_Rnanoflann_nn`, t(data), t(points), k, method, search, eps, square,
+                 sorted, radius, leafs, parallel, cores)
     if(trans){
         res$indices <- t(res$indices)
         res$distances <- t(res$distances)
