df46f7871ea56875db34325096e5636b7abd2303
Manos Papadakis <papadakm95@gmail.com>
2023-11-14 22:04:31 +0200

    Revert "Update nn.R"
    This reverts commit 1fde316095ab7ffa64e8f21c0e02262e69a32d38.

--- R/nn.R
+++ R/nn.R
@@ -1,12 +1,8 @@
 #[export]
 nn <- function(data, points = data, k = nrow(data), method = "euclidean", search = "standard", eps = 0.0, square = FALSE,
-                sorted = FALSE, radius = 0.0, trans = TRUE, leafs = 10, p = 0.0, parallel = FALSE, cores = 0){
-	if(method == "hellinger"){
-		data <- sqrt(data)
-		points <- sqrt(points)
-	}
+                sorted = FALSE, radius = 0.0, trans = TRUE, leafs = 10, parallel = FALSE, cores = 0){
     res <- .Call(`_Rnanoflann_nn`, t(data), t(points), k, method, search, eps, square,
-                 sorted, radius, leafs, p, parallel, cores)
+                 sorted, radius, leafs, parallel, cores)
     if(trans){
         res$indices <- t(res$indices)
         res$distances <- t(res$distances)
