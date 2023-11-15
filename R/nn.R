055399c65829c54a86c5538aca111cebdfd5532d
Manos Papadakis <papadakm95@gmail.com>
2023-11-15 16:41:34 +0200

    Update nn.R

--- R/nn.R
+++ R/nn.R
@@ -1,12 +1,11 @@
 #[export]
-nn <- function(data, points = data, k = nrow(data), method = "euclidean", search = "standard", eps = 0.0, square = FALSE,
-                sorted = FALSE, radius = 0.0, trans = TRUE, leafs = 10, p = 0.0, parallel = FALSE, cores = 0){
-	if(method == "hellinger"){
+nn <- function(data, points, k = nrow(data), method = "euclidean", search = "standard", 
+eps = 0.0, square = FALSE, sorted = FALSE, radius = 0.0, trans = TRUE, leafs = 10L, p = 0.0, parallel = FALSE, cores = 0L) {
+    if(method == "hellinger"){
 		data <- sqrt(data)
 		points <- sqrt(points)
 	}
-    res <- .Call(`_Rnanoflann_nn`, t(data), t(points), k, method, search, eps, square,
-                 sorted, radius, leafs, p, parallel, cores)
+    res <- .Call(`_Rnanoflann_nn`, data, points, k, method, search, eps, square, sorted, radius, leafs, p, parallel, cores)
     if(trans){
         res$indices <- t(res$indices)
         res$distances <- t(res$distances)
