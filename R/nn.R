aa1759e80993315598f22de7ddf4e1e33cbbd135
Manos Papadakis <papadakm95@gmail.com>
2023-11-16 13:28:36 +0200

    Update nn.R

--- R/nn.R
+++ R/nn.R
@@ -5,7 +5,7 @@ eps = 0.0, square = FALSE, sorted = FALSE, radius = 0.0, trans = TRUE, leafs = 1
 		data <- sqrt(data)
 		points <- sqrt(points)
 	}
-    res <- .Call(`_Rnanoflann_nn`, data, points, k, method, search, eps, square, sorted, radius, leafs, p, parallel, cores)
+    res <- .Call(`_Rnanoflann_nn`, t(data), t(points), k, method, search, eps, square, sorted, radius, leafs, p, parallel, cores)
     if(trans){
         res$indices <- t(res$indices)
         res$distances <- t(res$distances)
