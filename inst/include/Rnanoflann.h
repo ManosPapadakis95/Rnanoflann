5e0efb618117a865b2132d4d807b0927bb591a13
Manos Papadakis <papadakm95@gmail.com>
2024-12-13 11:19:41 +0200

    Update Rnanoflann.h

--- inst/include/Rnanoflann.h
+++ inst/include/Rnanoflann.h
@@ -2,3 +2,8 @@
 
 #include "internal/dists.hpp"
 #include "internal/KDTreeArmadilloAdaptor.hpp"
+
+List nn(arma::mat data, arma::mat points, arma::uword k, const std::string method = "euclidean",
+        const std::string search = "standard", const double eps = 0.0, const bool square = false,
+        const bool sorted = false, const double radius = 0.0, const unsigned int leafs = 10, const double p = 0.0,
+        const bool parallel = false, const unsigned int cores = 0);
